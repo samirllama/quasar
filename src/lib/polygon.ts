6fefb27b0fea8466136cadf1c2b2fca79bedd2ef
Samir Tamang <samirllama@gmail.com>
2025-08-25 10:46:10 -0500

    feat: Setup polygon client + lib/polygon.ts wrapper for consuming real-time data

---
+++ src/lib/polygon.ts
@@ -0,0 +1,10 @@
+//src/lib/polygon.ts
+import { restClient, websocketClient } from "@polygon.io/client-js";
+
+const apiKey = import.meta.env.VITE_POLYGON_API_KEY as string;
+
+// REST client for historical/aggregated data
+export const polygonRest = restClient(apiKey);
+
+// WebSocket client for live streaming (stocks, forex, etc.)
+export const polygonWS = websocketClient(apiKey);
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_POLYGON_API_KEY: string
+    // add VITE_* vars here
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
